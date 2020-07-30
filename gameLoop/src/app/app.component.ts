3f7998cd375565f4e0ca47f76052323b17d7dd12
Lisa2008 <zhang.lisa2008@gmail.com>
2020-07-30 13:12:42 +1000

    Game Loop game

---
+++ gameLoop/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('block1') block1: ElementRef;
+  @ViewChild('block2') block2: ElementRef;
+
+  
+}
